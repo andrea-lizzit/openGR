2dda4b29578615946cd7ffdd80e64d8f7a50305e
Andrea Lizzit <andrea.lizzit@gmail.com>
2023-05-23 15:31:58 +0200

    fix edge cases

--- compose_image.py
+++ compose_image.py
@@ -18,7 +18,8 @@ import scipy
 from tqdm import tqdm
 
 def sph2pix(map, width, height):
-	return map * np.array([height/np.pi, width / (2 * np.pi), 1])
+	closed_pixmap = map * np.array([(height)/np.pi, (width) / (2 * np.pi), 1])
+	return closed_pixmap[:-1, :-1, :]
 
 with open(args.map, 'r') as f:
 	map = np.array(json.load(f))
@@ -30,44 +31,29 @@ lcs = Image.open(args.lcs)
 aucs = np.array(ucs)
 alcs = np.array(lcs)
 
-def upsample(map, width, height, closed=False):
-	if closed:
-		width += 1
-		height += 1
+def upsample(map, width, height):
 	# upsample pixmap using map_coordinates
 	finegrid = np.array(np.meshgrid(np.arange(height), np.arange(width), np.arange(3), indexing='ij'))
-	upsample_map = finegrid * np.reshape(np.array([(map.shape[0] - 1) / (args.height-1), (map.shape[1] - 1) / (args.width-1), 1]), (3, 1, 1, 1))
+	upsample_map = finegrid * np.reshape(np.array([(map.shape[0] - 1) / (height-1), (map.shape[1] - 1) / (width-1), 1]), (3, 1, 1, 1))
 	upsampled_pixmap = ndimage.map_coordinates(map, upsample_map, order=1)
-	if closed:
-		upsampled_pixmap = upsampled_pixmap[:, :-1, :-1]
 	return upsampled_pixmap
 
 
 def warp_image(ucs, lcs, map):
 	# convert spherical coordinates to pixel coordinates
+	if args.height is not None and args.width is not None:
+		map = upsample(map, args.width+1, args.height+1) # +1 because the edges will be discarded
 	ucs_pixmap = sph2pix(map, ucs.shape[1], ucs.shape[0])
-	# if args.width is None:
-	# 	args.width = ucs.shape[1]
-	# if args.height is None:
-	# 	args.height = ucs.shape[0]
-	if args.height is not None and args.width is not None:
-		ucs_pixmap = upsample(ucs_pixmap, args.width, args.height)
-	ucs_pixmap = ucs_pixmap.transpose((2, 0, 1))
 	lcs_pixmap = sph2pix(map, lcs.shape[1], lcs.shape[0])
-	# if args.width is None:
-	# 	args.width = lcs.shape[1]
-	# if args.height is None:
-	# 	args.height = lcs.shape[0]
-	if args.height is not None and args.width is not None:
-		lcs_pixmap = upsample(lcs_pixmap, args.width, args.height)
-	lcs_pixmap = lcs_pixmap.transpose((2, 0, 1))
+	ucs_pixmap = (ucs_pixmap.transpose((2, 0, 1)))
+	lcs_pixmap = (lcs_pixmap.transpose((2, 0, 1)))
 	lcs_pixmap = np.flip(lcs_pixmap, axis=2)
 	ucs_channels, lcs_channels = np.split(ucs, 3, axis=2), np.split(lcs, 3, axis=2)
 	warped_channels = []
 	for uc, lc in zip(ucs_channels, lcs_channels):
 		uc, lc = np.squeeze(uc), np.squeeze(lc)
-		warped_uc = ndimage.map_coordinates(uc, ucs_pixmap[:2], order=1)
-		warped_lc = ndimage.map_coordinates(lc, lcs_pixmap[:2], order=1)
+		warped_uc = ndimage.map_coordinates(uc, ucs_pixmap[:2], order=1, cval=127, mode="grid-wrap")
+		warped_lc = ndimage.map_coordinates(lc, lcs_pixmap[:2], order=1, cval=127, mode="grid-wrap")
 		mask = np.rint(ucs_pixmap[2]).astype(np.uint8)
 		warped_skies = warped_uc * mask + warped_lc * (1 - mask)
 		warped_channels.append(warped_skies)
